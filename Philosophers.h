bb00d35d55ffb053a5863339db55b7c64f58a8ca
Hicham Meftah <hmeftah@c1r6p5.1337.ma>
2023-02-28 16:37:27 +0100

    I hate mountassir

---
+++ Philosophers.h
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Philosophers.h                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hmeftah <hmeftah@student.1337.ma>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/28 16:24:00 by hmeftah           #+#    #+#             */
+/*   Updated: 2023/02/28 16:35:09 by hmeftah          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILOSOPHERS_H
+# define PHILOSOPHERS_H
+
+# include <stdio.h>
+# include <string.h>
+# include <stdlib.h>
+# include <pthread.h>
+
+
+struct s_philo {
+	int	id;
+	
+};
+
+#endif
